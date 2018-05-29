ea81d1b240f6138db130d0500967207f2d8ba8b7
mitle <mitnyik.levente@hallgato.ppke.hu>
2018-05-29 02:57:52 +0200

    Communicator class

--- server.py
+++ server.py
@@ -7,23 +7,118 @@ from cryptography.fernet import Fernet
 import pickle
 import rsa
 
+import socket
+
+
+class Communicator():
+    SERVER = 'ser'
+    CLIENT = 'cli'
+    def __init__(self, mode):
+        if mode in [self.SERVER, self.CLIENT]:
+            self.mode = mode
+        else:
+            raise ValueError
+
+        if self.mode == self.SERVER:
+            self.__init_server()
+        elif self.mode == self.CLIENT:
+            self.__init_client()
+
+    def init_encryption(self):
+        if self.mode == self.SERVER:
+            self.__init_server_encryption()
+        elif self.mode == self.CLIENT:
+            self.__init_client_encryption()
+
+    def __init_server(self):
+        self.ip_text = 'localhost'
+        self.port = "14522"
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PAIR)
+        self.socket.bind("tcp://*:{port}".format(port=self.port))
+
+    def __init_client(self):
+        pass
+
+    def __init_server_encryption(self):
+        (self.pubkey, self.privkey) = rsa.newkeys(1024)
+        self.send(self.pubkey, pyobj=True)
+
+        # get sym auth
+        try:
+            encrypted_key = self.nonblock_recv(timeout=15)
+        except fir.Timeout as e:
+            raise e
+
+        self.symm_key = rsa.decrypt(encrypted_key, self.privkey)
+        self.symmetric_cipher_f = Fernet(self.symm_key)
+
+    def __init_client_encryption(self):
+        pass
+
+    def encrypted_send(self, data):
+        token = self.symmetric_cipher_f.encrypt(pickle.dumps(data))
+        print("sending data: ", end='')
+        print(data)
+        self.send(token)
+
+    def encrypted_recv(self):
+        recv_data = self.nonblock_recv()
+        if recv_data is None:
+            return None
+        pickeled_data = self.symmetric_cipher_f.decrypt(recv_data)
+        data = pickle.loads(pickeled_data)
+        print(data)
+        return data
+
+    def nonblock_recv(self, timeout=0.0, pyobj=False):
+        if timeout == 0.0:
+            try:
+                if pyobj:
+                    data = self.socket.recv_pyobj(flags=zmq.NOBLOCK)
+                else:
+                    data = self.socket.recv(flags=zmq.NOBLOCK)
+                return data
+            except zmq.Again as e:
+                return None
+        else:
+            start = time.time()
+            timeout_sec = timeout
+            graceful = False
+            while time.time() - start < timeout_sec:
+                try:
+                    if pyobj:
+                        data = self.socket.recv_pyobj(flags=zmq.NOBLOCK)
+                    else:
+                        data = self.socket.recv(flags=zmq.NOBLOCK)
+
+                    return data
+                except zmq.Again as e:
+                    time.sleep(.99)
+
+            print("Connection timed out!")
+            raise fir.Timeout
+
+    def send(self, data, pyobj=False):
+        if pyobj:
+            self.socket.send_pyobj(data)
+        else:
+            self.socket.send(data)
+
+
+
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((640, 640))
-font = pygame.font.SysFont("Courier New", 24)
 done = False
 
+
+comm = Communicator(Communicator.SERVER)
+
+ip_list = socket.gethostbyname_ex(socket.gethostname())[2]
+
+
 is_connected = False
-
-#import socket
-#ip_text = socket.gethostbyname(socket.gethostname())
-
-ip_text = 'localhost'
-port = "14522"
-context = zmq.Context()
-socket = context.socket(zmq.PAIR)
-socket.bind("tcp://*:{port}".format(port=port))
-
 while not done and not is_connected:
     screen.fill((42, 42, 42))
     for event in pygame.event.get():
@@ -31,30 +126,28 @@ while not done and not is_connected:
             done = True
 
     # print title
+    font = pygame.font.SysFont("Courier New", 24)
     txt_surface = font.render("Your IP address is: ", True, pygame.Color('azure2'))
     screen.blit(txt_surface, (5, 15))
 
     # Render the current text.
-    txt_surface = font.render(ip_text, True, pygame.Color('azure2'))
+    y = 55
+    for ipaddr in ip_list:
+        txt_surface = font.render(ipaddr, True, pygame.Color('azure2'))
+        screen.blit(txt_surface, (55, y))
+        y += 32
     #txt_surface = font.render(ip_text + ':' + port, True, pygame.Color('azure2'))
 
-    try:
-        msg = socket.recv(flags=zmq.NOBLOCK)
-        if msg == b"hello_fir_server":
-            print("Incoming connection")
-            is_connected = True
-        else:
-            print("header mismatch: " + msg)
-    except zmq.Again as e:
-        pass
-
-    screen.blit(txt_surface, (55, 55))
+    msg = comm.nonblock_recv()
+    if msg is not None and msg == b"hello_fir_server":
+        print("Incoming connection")
+        is_connected = True
+    elif msg is not None:
+        print("header mismatch: " + msg)
 
     pygame.display.flip()
     clock.tick(25)
 
-(pubkey, privkey) = rsa.newkeys(1024)
-socket.send_pyobj(pubkey)
 
 class Player():
     def __init__(self, name, color):
@@ -64,37 +157,15 @@ class Player():
         self.points = 0
         self.last_move = None
 
+comm.init_encryption()
 
-#get sym auth
-start = time.time()
-timeout_sec = 15
-graceful = False
-while time.time() - start < timeout_sec:
-    try:
-        encrypted_key = socket.recv(flags=zmq.NOBLOCK)
-        graceful = True
-        break
-    except zmq.Again as e:
-        time.sleep(.2)
-if not graceful:
-    print("Connection timed out!")
-    raise fir.Timeout
-
-key = rsa.decrypt(encrypted_key, privkey)
-f = Fernet(key)
-
-enc_msg = socket.recv()
-pickeled_player = f.decrypt(enc_msg)
-player1 = pickle.loads(pickeled_player)
+player1 = comm.encrypted_recv()
 
 player2 = Player("Levi", "white")
 
 print(player1.name)
 
-
-token = f.encrypt(pickle.dumps(player2))
-print("sending player: " + player2.name)
-socket.send(token)
+comm.encrypted_send( player2)
 
 
 #recieving configuration
@@ -109,17 +180,7 @@ for c in confs:
     conf[c] = recv_conf[c]
 
 
-def encrypted_recv():
-    try:
-        recv_data = socket.recv(flags=zmq.NOBLOCK)
-        pickeled_data = f.decrypt(recv_data)
-        data = pickle.loads(pickeled_data)
-        print(data)
-        return data
-    except zmq.Again as e:
-        return None
-
-
+font = pygame.font.SysFont(None, 24)
 
 while not done:
     screen.fill((42, 42, 42))
@@ -137,7 +198,7 @@ while not done:
     txt_surface = font.render("you    : " + player2.name, True, pygame.Color('red'))
     screen.blit(txt_surface, (150, 200))
 
-    data = encrypted_recv()
+    data = comm.encrypted_recv()
 
     pygame.display.flip()
     clock.tick(25)
